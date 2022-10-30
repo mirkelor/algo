8b34da1bad52215cab4fca18de8ce33b7ec55faa
Hasan Efe <hasanefee@hotmail.com>
2022-10-30 17:08:41 +0300

    subtree of another tree added

---
+++ leetcode/blind75/0572.SubtreeOfAnotherTree.go
@@ -0,0 +1,23 @@
+package blind75
+
+import "github.com/Mirkelor/algo/common"
+
+func isSubtree(root *common.TreeNode, subRoot *common.TreeNode) bool {
+	if root == nil {
+		return false
+	}
+	if isSubtreeHelper(root, subRoot) {
+		return true
+	}
+	return isSubtree(root.Left, subRoot) || isSubtree(root.Right, subRoot)
+}
+
+func isSubtreeHelper(p *common.TreeNode, q *common.TreeNode) bool {
+	if p == nil && q == nil {
+		return true
+	}
+	if p == nil || q == nil || p.Val != q.Val {
+		return false
+	}
+	return isSubtreeHelper(p.Left, q.Left) && isSubtreeHelper(p.Right, q.Right)
+}
---
+++ leetcode/blind75/0572.SubtreeOfAnotherTree_test.go
@@ -0,0 +1,23 @@
+package blind75
+
+import (
+	"testing"
+
+	"github.com/Mirkelor/algo/common"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSubtreeOfAnotherTree(t *testing.T) {
+	tests := []struct {
+		in1      *common.TreeNode
+		in2      *common.TreeNode
+		expected bool
+	}{
+		{common.NewTreeNode(3, 4, 5, 1, 2), common.NewTreeNode(4, 1, 2), true},
+		{common.NewTreeNode(3, 4, 5, 1, 2, -1, -1, -1, -1, 0), common.NewTreeNode(4, 1, 2), false},
+	}
+
+	for _, tt := range tests {
+		assert.Equal(t, tt.expected, isSubtree(tt.in1, tt.in2))
+	}
+}
