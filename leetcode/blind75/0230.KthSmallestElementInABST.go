cab9e784f4afcdce5abad81945933609fa19770f
Hasan Efe <hasanefee@hotmail.com>
2022-11-18 20:08:01 +0300

    kth smallest element in a bst added

---
+++ leetcode/blind75/0230.KthSmallestElementInABST.go
@@ -0,0 +1,24 @@
+package blind75
+
+import "github.com/Mirkelor/algo/common"
+
+func kthSmallest(root *common.TreeNode, k int) int {
+	res := -1
+	count := 0
+	var dfs func(*common.TreeNode)
+	dfs = func(node *common.TreeNode) {
+		if node == nil {
+			return
+		}
+		dfs(node.Left)
+		if count++; count == k {
+			res = node.Val
+			return
+		} else if count > k {
+			return
+		}
+		dfs(node.Right)
+	}
+	dfs(root)
+	return res
+}
---
+++ leetcode/blind75/0230.KthSmallestElementInABST_test.go
@@ -0,0 +1,23 @@
+package blind75
+
+import (
+	"testing"
+
+	"github.com/Mirkelor/algo/common"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestKthSmallestElementInABST(t *testing.T) {
+	tests := []struct {
+		in1      *common.TreeNode
+		in2      int
+		expected int
+	}{
+		{common.NewTreeNode(3, 1, 4, -1, 2), 1, 1},
+		{common.NewTreeNode(5, 3, 6, 2, 4, -1, -1, 1), 3, 3},
+	}
+
+	for _, tt := range tests {
+		assert.Equal(t, tt.expected, kthSmallest(tt.in1, tt.in2))
+	}
+}
