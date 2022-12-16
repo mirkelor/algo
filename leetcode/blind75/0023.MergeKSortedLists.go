0f7c27d51cbb688e4cb59f6c9cc5706c6a54f90a
Hasan Efe <hasanefee@hotmail.com>
2022-12-16 19:53:23 +0300

    merge k sorted lists added

---
+++ leetcode/blind75/0023.MergeKSortedLists.go
@@ -0,0 +1,45 @@
+package blind75
+
+import "github.com/Mirkelor/algo/common"
+
+func mergeKLists(lists []*common.ListNode) *common.ListNode {
+	if len(lists) == 0 {
+		return nil
+	}
+
+	for len(lists) > 1 {
+		mergedLists := make([]*common.ListNode, 0)
+		for i := 0; i < len(lists); i += 2 {
+			if i+1 < len(lists) {
+				mergedLists = append(mergedLists, mergeList(lists[i], lists[i+1]))
+			} else {
+				mergedLists = append(mergedLists, mergeList(lists[i], nil))
+			}
+		}
+		lists = mergedLists
+	}
+
+	return lists[0]
+}
+
+func mergeList(l1, l2 *common.ListNode) *common.ListNode {
+	merged := new(common.ListNode)
+	tail := merged
+	for l1 != nil && l2 != nil {
+		if l1.Val < l2.Val {
+			tail.Next, l1 = l1, l1.Next
+		} else {
+			tail.Next, l2 = l2, l2.Next
+		}
+		tail = tail.Next
+	}
+
+	if l1 == nil {
+		tail.Next = l2
+	}
+	if l2 == nil {
+		tail.Next = l1
+	}
+
+	return merged.Next
+}
---
+++ leetcode/blind75/0023.MergeKSortedLists_test.go
@@ -0,0 +1,23 @@
+package blind75
+
+import (
+	"testing"
+
+	"github.com/Mirkelor/algo/common"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMergeKSortedLists(t *testing.T) {
+	tests := []struct {
+		in       []*common.ListNode
+		expected *common.ListNode
+	}{
+		{[]*common.ListNode{common.NewListNode(1, 4, 5), common.NewListNode(1, 3, 4), common.NewListNode(2, 6)}, common.NewListNode(1, 1, 2, 3, 4, 4, 5, 6)},
+		{[]*common.ListNode{}, common.NewListNode()},
+		{[]*common.ListNode{common.NewListNode()}, common.NewListNode()},
+	}
+
+	for _, tt := range tests {
+		assert.Equal(t, tt.expected, mergeKLists(tt.in))
+	}
+}
