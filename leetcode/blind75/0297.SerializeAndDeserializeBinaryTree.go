c38cd195a229ff97ecc04627efec50531128e127
Hasan Efe <hasanefee@hotmail.com>
2022-12-18 13:35:21 +0300

    serialize and deserialize binary tree added

---
+++ leetcode/blind75/0297.SerializeAndDeserializeBinaryTree.go
@@ -0,0 +1,53 @@
+package blind75
+
+import (
+	"strconv"
+	"strings"
+
+	"github.com/Mirkelor/algo/common"
+)
+
+type Codec struct {
+}
+
+func Constructor() Codec {
+	return Codec{}
+}
+
+// Serializes a tree to a single string.
+func (c *Codec) serialize(root *common.TreeNode) string {
+	res := make([]string, 0)
+	var dfs func(node *common.TreeNode)
+	dfs = func(node *common.TreeNode) {
+		if node == nil {
+			res = append(res, "N")
+			return
+		}
+		res = append(res, strconv.Itoa(node.Val))
+		dfs(node.Left)
+		dfs(node.Right)
+	}
+	dfs(root)
+	return strings.Join(res, ",")
+}
+
+// Deserializes your encoded data to tree.
+func (c *Codec) deserialize(data string) *common.TreeNode {
+	values := strings.Split(data, ",")
+	i := 0
+	var dfs func() *common.TreeNode
+	dfs = func() *common.TreeNode {
+		if values[i] == "N" {
+			i++
+			return nil
+		}
+		val, _ := strconv.Atoi(values[i])
+		i++
+		node := &common.TreeNode{
+			Val:   val,
+			Left:  dfs(),
+			Right: dfs()}
+		return node
+	}
+	return dfs()
+}
---
+++ leetcode/blind75/0297.SerializeAndDeserializeBinaryTree_test.go
@@ -0,0 +1,24 @@
+package blind75
+
+import (
+	"testing"
+
+	"github.com/Mirkelor/algo/common"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSerializeAndDeserializeBinaryTree(t *testing.T) {
+	tests := []struct {
+		in       *common.TreeNode
+		expected *common.TreeNode
+	}{
+		{common.NewTreeNode(1, 2, 3, -1, -1, 4, 5), common.NewTreeNode(1, 2, 3, -1, -1, 4, 5)},
+		{common.NewTreeNode(1, 2, 3, -1, -1, 4, 5, -1, -1, -1, -1, 6, 7), common.NewTreeNode(1, 2, 3, -1, -1, 4, 5, -1, -1, -1, -1, 6, 7)},
+		{nil, nil},
+	}
+
+	for _, tt := range tests {
+		c := Codec{}
+		assert.Equal(t, tt.expected, c.deserialize(c.serialize(tt.in)))
+	}
+}
